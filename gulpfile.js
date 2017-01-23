1b802ee6a9fe2f803e05acc1ab8d78df02d8eef8
Dimitra Zuccarelli <dimitrazuccarelli@gmail.com>
2017-01-23 09:56:39 +0000

    Placeholder site

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var cssnano = require('gulp-cssnano');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require("gulp-rename");
+
+// Live browser reloading
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+});
+
+// Watch
+gulp.task('watch', ['browserSync'], function() {
+    // Refresh the browser if any HTML changes occur
+    gulp.watch('*.html', browserSync.reload);
+});
+
+// Minify CSS
+gulp.task('minify', function() {
+    return gulp.src('css/main.css')
+        .pipe(cssnano())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+// And finally, build tasks
+gulp.task('default', function (callback) {
+    runSequence(['browserSync', 'watch'],
+        callback
+    );
+});
+
+gulp.task('build', function (callback) {
+    runSequence('minify',
+        callback
+    );
+});
